777301a9ff5f7c043c4ac1971ad1a71c953b99d6
fernando-lorandi <fernandolorand@gmail.com>
2025-05-05 18:23:14 -0300

    Criação de functions para entrada e saída de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,59 +13,56 @@ typedef struct {
 
 } CartaSuperTrunfo;
 
-int main() {
-    CartaSuperTrunfo cartas[2]; //Chamando a classe e definindo o array de cartas
+//Função de inserção de dados, recebe a referência da struct para de cartas e a posição dos dados no array;
+int cadastroDeCartas(CartaSuperTrunfo cartas[], int i) {
 
-    //Carta #01
-    printf("\nCarta #01 \n");
+    printf("\nCadastro de carta #0%d:\n", i);
     printf("Selecione um estado (A-H): ");
-    scanf(" %c", &cartas[0].estado);
+    scanf(" %c", &cartas[i].estado);
     printf("Selecione um codigo de caracteres (1-4): ");
-    scanf("%s", cartas[0].codigoCarta);
+    scanf("%s", cartas[i].codigoCarta);
     printf("Digite o nome da cidade: ");
-    fscanf(stdin, " %[^\n]", cartas[0].nomeCidade);
+    fscanf(stdin, " %[^\n]", cartas[i].nomeCidade);
     printf("Informe a população: ");
-    scanf("%f", &cartas[0].populacao);
+    scanf("%f", &cartas[i].populacao);
     printf("Informe a área (em Km²): ");
-    scanf("%f", &cartas[0].area);
+    scanf("%f", &cartas[i].area);
     printf("Informe o PIB: ");
-    scanf("%f", &cartas[0].PIB);
+    scanf("%f", &cartas[i].PIB);
     printf("Informe o número de pontos turísticos: ");
-    scanf("%d", &cartas[0].pontosTuristicos);
-
-    //Carta #02
-    printf("\nCarta #02 \n");
-    printf("Selecione um estado (A-H): ");
-    scanf(" %c", &cartas[1].estado);
-    printf("Selecione um codigo de caracteres (1-4): ");
-    scanf("%s", cartas[1].codigoCarta);
-    printf("Digite o nome da cidade: ");
-    fscanf(stdin, " %[^\n]", cartas[1].nomeCidade);
-    printf("Informe a população: ");
-    scanf("%f", &cartas[1].populacao);
-    printf("Informe a área (em Km²): ");
-    scanf("%f", &cartas[1].area);
-    printf("Informe o PIB: ");
-    scanf("%f", &cartas[1].PIB);
-    printf("Informe o número de pontos turísticos: ");
-    scanf("%d", &cartas[1].pontosTuristicos);
-
-
-    printf("\nResumo #01:\n");
-    printf("Código: %c0%s\n", cartas[0].estado, cartas[0].codigoCarta );
-    printf("Cidade: %s\n", cartas[0].nomeCidade);
-    printf("População: %.2f habitantes\n", cartas[0].populacao);
-    printf("Área: %.2f Km²\n", cartas[0].area);
-    printf("PIB: %.2f bilhões de reais \n", cartas[0].PIB);
-    printf("Número de pontos turísticos: %d\n", cartas[0].pontosTuristicos);
-
-    printf("\nResumo #02:\n");
-    printf("Código: %c0%s\n", cartas[1].estado, cartas[1].codigoCarta );
-    printf("Cidade: %s\n", cartas[1].nomeCidade);
-    printf("População: %.2f habitantes\n", cartas[1].populacao);
-    printf("Área: %.2f Km²\n", cartas[1].area);
-    printf("PIB: %.2f bilhões de reais \n", cartas[1].PIB);
-    printf("Número de pontos turísticos: %d\n", cartas[1].pontosTuristicos);
+    scanf("%d", &cartas[i].pontosTuristicos);
 
     return 0;
+}
+
+//Função de impressão dos dados, recebe a referência da struct para de cartas e a posição dos dados no array;
+int impressaoDeCartas(CartaSuperTrunfo cartas[], int i) {
+
+    printf("\nResumo #0%d:\n", i);
+    printf("Código: %c0%s\n", cartas[i].estado, cartas[i].codigoCarta );
+    printf("Cidade: %s\n", cartas[i].nomeCidade);
+    printf("População: %.2f habitantes\n", cartas[i].populacao);
+    printf("Área: %.2f Km²\n", cartas[i].area);
+    printf("PIB: %.2f bilhões de reais \n", cartas[i].PIB);
+    printf("Número de pontos turísticos: %d\n", cartas[i].pontosTuristicos);
+
+
+    return 0;
+}
+
+int main() {
+    //Chama a struck e passa 3 arrays.
+    //Ignorei a posição 0 para não existir nenhuma carta com "ID" #00;
+    CartaSuperTrunfo cartas[3];
+
+    cadastroDeCartas(cartas, 1);
+    cadastroDeCartas(cartas, 2);
+
+    impressaoDeCartas(cartas, 1);
+    impressaoDeCartas(cartas, 2);
+
+
+    return 0;
+
+
 }
